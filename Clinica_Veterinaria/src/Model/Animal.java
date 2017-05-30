3b3e65509e7dd185904df66916f92338e88962d9
d119322 <d119322@LI022>
2017-05-29 21:39:29 -0300

    Primeiras classes

---
+++ Clinica_Veterinaria/src/Model/Animal.java
@@ -0,0 +1,28 @@
+    /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author d119322
+ */
+public class Animal {
+    
+    private String nome_animal;
+    private int idade_animal;
+    private int sexo_animal;
+    
+    public String Vis_Animal(){
+        return "";
+       
+    }
+    
+    public String Con_Animal(){
+        return "";
+    }
+    
+    
+}
---
+++ Clinica_Veterinaria/src/Model/Cliente.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author d119322
+ */
+public class Cliente {
+    
+    private String nom_cli;
+    private String end_cli;
+    private String tel_cli;
+    private long cep_cli;
+    private String email_cli;
+    
+    public int Reg_Cli(){
+    
+    return 0;
+    };
+    
+    public String Con_cli(){
+        
+        return "";
+    }
+    
+}
